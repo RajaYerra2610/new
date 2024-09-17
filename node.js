d728f784b02f8ffb7bd5e446106c1bdfa7f8f214
RajaYerra2610 <123198967+RajaYerra2610@users.noreply.github.com>
2024-09-17 12:25:17 +0530

    Add files via upload

--- node.js
+++ node.js
@@ -1,27 +1,11 @@
-const express = require("express");
-const path = require("path");
-
-const { open } = require("sqlite");
-const sqlite3 = require("sqlite3");
+const express = require('express');
 const app = express();
+const port = process.env.PORT || 3000;
 
-const dbPath = path.join(__dirname, "goodreads.db");
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
 
-let db = null;
-
-const initializeDBAndServer = async () => {
-  try {
-    db = await open({
-      filename: dbPath,
-      driver: sqlite3.Database,
-    });
-    app.listen(3000, () => {
-      console.log("Server Running at http://localhost:3000/");
-    });
-  } catch (e) {
-    console.log(`DB Error: ${e.message}`);
-    process.exit(1);
-  }
-};
-
-initializeDBAndServer();
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
